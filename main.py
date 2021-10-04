de53112ae7beca97fd076f9f4e807665d8c88d16
Deedss <gertjan.rolink@outlook.com>
2021-10-04 17:13:28 +0200

    initial setup

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# This will be a note taking application on the commandline written fully in Python
+
+def main():
+    print("hello world")
+
+if __name__ == '__main__':
+    main()
